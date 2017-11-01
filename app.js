af5ea22d7601a6efc9269c5cc0c17cdf87faba39
BarisYazici <barisyazici@sabanciuniv.edu>
2017-11-01 22:06:59 +0100

    change the post method url to production url

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ setInterval(function(){request.get(options, (error, response, body) => {
 
 	console.log(gpsLat, gpsLng)
 	postConfig = {
-		url: 'http://localhost:3000/api/bmwdata',
+		url: 'https://bemostwanted.herokuapp.com/api/bmwdata',
 		form: data 
 	}
 	console.log(postConfig)
