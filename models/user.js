f78a5edafe63656560a7048a3676874d42fcf824
BarisYazici <barisyazici@sabanciuniv.edu>
2017-11-19 23:38:03 +0100

    User model and schema added

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+//User Schema
+const UserSchema = mongoose.Schema({
+	name: {
+		type: string,
+		required: true
+	},
+	email: {
+		type: string,
+		required: true
+	},
+	username: {
+		type: string,
+		required: true
+	},
+	password: {
+		type: string,
+		required: true
+	},
+})
+
+const User = module.exports = mongoose.model('User', UserSchema);
