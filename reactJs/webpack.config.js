ca6613babf45a78b1d7403f9ed074bbfdcb21226
Diegogabo <diego.gaboardi@mail.polimi.it>
2017-11-21 14:17:07 +0100

    configuring webpack.config.js

--- reactJs/webpack.config.js
+++ reactJs/webpack.config.js
@@ -1,4 +1,17 @@
 module.exports = {
-	entry: __dirname + '/app/index.js'
+	entry: __dirname + '/app/index.js',
+	module: {
+		loaders: [
+		{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: 'babel-loader'
+		}
+	]
+	},
+	output: {
+		filename: 'trasformed.js,
+		path: __dirname + '/build'
+	}
 };
 
