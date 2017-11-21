a09db22639eb7e6374cdb1c0342a3ceccbedfc81
Diegogabo <diego.gaboardi@mail.polimi.it>
2017-11-21 15:07:02 +0100

    hello word for react-js

--- reactJs/webpack.config.js
+++ reactJs/webpack.config.js
@@ -1,3 +1,10 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+	template: __dirname + '/app/index.html',
+	filename: 'index.html',
+	inject: 'body'
+});
+
 module.exports = {
 	entry: __dirname + '/app/index.js',
 	module: {
@@ -7,11 +14,12 @@ module.exports = {
 			exclude: /node_modules/,
 			loader: 'babel-loader'
 		}
-	]
+		]
 	},
 	output: {
-		filename: 'trasformed.js,
+		filename: 'transformed.js',
 		path: __dirname + '/build'
-	}
+	},
+	plugins: [HTMLWebpackPluginConfig]
 };
 
