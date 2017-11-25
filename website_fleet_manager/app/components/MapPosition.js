aaa988dd12365a8d4f1c43aca795ae28ac606bdb
Diegogabo <diego.gaboardi@mail.polimi.it>
2017-11-25 11:05:19 +0100

    google map added in react js

--- website_fleet_manager/app/components/MapPosition.js
+++ website_fleet_manager/app/components/MapPosition.js
@@ -1,12 +1,60 @@
 import React from 'react';
+import {Gmaps, Marker, InfoWindow, Circle} from 'react-gmaps';
+
+const coords = {
+  lat:  48.161502,
+  lng: 11.524362
+};
+
+const params = {v: '3.exp', key: 'AIzaSyCvFKFLNyeslv9r5VdA86Vnu1-e-E_-HQ4'};
 
 export class MapPosition extends React.Component {
-  
+
+  onMapCreated(map) {
+    map.setOptions({
+      disableDefaultUI: true
+    });
+  }
+
+  onDragEnd(e) {
+    console.log('onDragEnd', e);
+  }
+
+  onCloseClick() {
+    console.log('onCloseClick');
+  }
+
+  onClick(e) {
+    console.log('onClick', e);
+  }
+
   render() {
     return (
-      <div>
-        map
-      </div>
+      <Gmaps
+        width={'100%'}
+        height={'300px'}
+        lat={coords.lat}
+        lng={coords.lng}
+        zoom={15}
+        params={params}
+        onMapCreated={this.onMapCreated}>
+        <Marker
+          lat={coords.lat}
+          lng={coords.lng}
+          draggable={true}
+          onDragEnd={this.onDragEnd} />
+        <InfoWindow
+          lat={coords.lat}
+          lng={coords.lng}
+          content={'Last Position of the car'}
+          onCloseClick={this.onCloseClick} />
+        <Circle
+          lat={coords.lat}
+          lng={coords.lng}
+          radius={25}
+          onClick={this.onClick} />
+      </Gmaps>
     );
   }
-}
+
+};
