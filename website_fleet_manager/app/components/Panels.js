fef4ae120bcac5fbecff296758e51198c8dcc8c1
Diegogabo <diego.gaboardi@mail.polimi.it>
2017-11-25 11:06:19 +0100

    panels component added in react js

--- website_fleet_manager/app/components/Panels.js
+++ website_fleet_manager/app/components/Panels.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import '../css/panels.css';
+
+import  {StarPanel} from './StarPanel';
+import  {FuelPanel} from './FuelPanel';
+
 export class Panels extends React.Component {
   
   render() {
     return (
       <div>
-        Panels
+        <div className="w3-container">
+          <ul className="w3-ul w3-card-4"> 
+            <StarPanel />
+            <StarPanel /> 
+            <FuelPanel />
+          </ul>
+        </div>
       </div>
     );
   }
