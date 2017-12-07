464a55f68e687cfb9523a8ae4b847392c0691ec4
Diegogabo <diego.gaboardi@mail.polimi.it>
2017-12-07 09:44:44 +0100

    data are rendered dinamically

--- src/app/components/FilterMenu.js
+++ src/app/components/FilterMenu.js
@@ -3,7 +3,8 @@
 import React from 'react';
 
 export class FilterMenu extends React.Component {
-
+    
+  
   render() {
     return (
       <div>
@@ -11,10 +12,10 @@ export class FilterMenu extends React.Component {
           <div className="col-sm-3" style={{display: 'inline'}}><label>{this.props.title}:</label></div>
           <div className="col-sm-9">
             <select className="form-control" onChange={this.props.handleChange}>
-            <option>{this.props.v1}</option>
-            <option>{this.props.v2}</option>
-            <option>{this.props.v3}</option>
-            <option>{this.props.v4}</option>
+            <option value={this.props.v1}>{this.props.v1}</option>
+            <option value={this.props.v2}>{this.props.v2}</option>
+            <option value={this.props.v3}>{this.props.v3}</option>
+            <option value={this.props.v4}>{this.props.v4}</option>
           </select>
           </div>
         </div>
