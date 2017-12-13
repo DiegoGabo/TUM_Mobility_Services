2069fb04de3b9a5c93a8c519196ad0b7efce2e64
Lukas Kick <lukaskick1994@gmail.com>
2017-12-13 18:40:52 +0100

    Cost Calculation Panel
    - Adapted CSS file
    - created chartjs

--- src/app/components/CalcolationPanel.js
+++ src/app/components/CalcolationPanel.js
@@ -1,11 +1,50 @@
 import React from 'react';
 
+import '../css/calcolationPanel.css';
+import {Bar, Line, Pie} from 'react-chartjs-2';
+
 export class CalcolationPanel extends React.Component {
+
+  constructor(props){
+      super(props);
+      this.state = {
+        chartData:{
+          labels: ['Mentainance Costs', 'Fuel Costs'],
+          datasets:[
+            {
+              label:'2016',
+              backgroundColor: "#3B3838",
+              data: [400,100],
+            }, {
+              label:'2017',
+              backgroundColor: "#00B0F0",
+              data: [370,95],
+            }
+          ]
+        }
+      }
+  }
 
   render() {
     return (
-      <div>
-        calcolation panel
+      <div className="filter_panel_calcolation">
+      <li className="w3-bar panel">
+        <div className="row">
+          <h3 className="kpi_title">Cost Calculation</h3>
+        </div>
+        <div className="row calcolationRow">
+          <Bar
+            data= {this.state.chartData}
+            options={{
+            title:{
+              display:true,
+              text:'Overall variable costs (in thousand euro)',
+              fontSize:10
+            }
+            }}
+          />
+        </div>
+      </li>
       </div>
     );
   }
