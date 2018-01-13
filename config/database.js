859de9e7fb961bb81165c94760aa1930bbf4a04d
BarisYazici <barisyazici@sabanciuniv.edu>
2018-01-13 15:40:14 +0100

    working on a remote database. Not local database anymore

--- config/database.js
+++ config/database.js
@@ -7,8 +7,8 @@ if (process.env.NODE_ENV === 'production') {
 }
 else{
 	module.exports={
-		database: 'mongodb://localhost/bmw7data',
-		//database:'mongodb://heroku_w5z3d9qq:977j9v5ee163bc59p3l1l96a3f@ds243335.mlab.com:43335/heroku_w5z3d9qq',
+		//database: 'mongodb://localhost/bmw7data',
+		database:'mongodb://heroku_w5z3d9qq:977j9v5ee163bc59p3l1l96a3f@ds243335.mlab.com:43335/heroku_w5z3d9qq',
 		url: 'http://localhost:3000/'
 	}
 
